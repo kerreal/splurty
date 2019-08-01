54731b9723dbecf5ee621cbdc0a5b3400ce03859
kerreal <weareme99@gmail.com>
2019-08-01 03:36:49 +0000

    Author spelled correctly

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2019_08_01_021729) do
 
   create_table "quotes", force: :cascade do |t|
     t.string "saying"
-    t.string "auther"
+    t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
